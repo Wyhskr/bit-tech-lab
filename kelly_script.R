4fb4e3492fdb2d47d4b330721547b834d69c6a15
Alexcalabrisello <12953131@student.uts.edu.au>
2018-08-22 12:14:07 +1000

    Alex's version of Kelly's script

--- kelly_script.R
+++ kelly_script.R
@@ -1,13 +1,13 @@
-# install.packages("tidyverse")
+install.packages("tidyverse")
 
 library(tidyverse)
 
 
 diamonds2 <- diamonds %>% 
-  filter(color == G & depth > 60) %>% 
+  filter(color == "G" & depth > 60) %>% 
   group_by(clarity) %>% 
   arrange(clarity) %>% 
-  mutate(av_carat = mean(carot))
+  mutate(av_carat = mean(carat))
 
-ggplot(diamonds2, aes(car0t, price)) %>% 
-  geom_point(aes(colour=cut size=table, alpha=1/10)) +
+ggplot(diamonds2, aes(carat, price)) +
+  geom_point(aes(colour=cut, size=table, alpha=1/10)) 
